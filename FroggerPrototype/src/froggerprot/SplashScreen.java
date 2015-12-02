6bd1d945bf843d4d68369a139cbe96b8fbec77ae
mpw5216 <mpw5216@206-L64.up.ist.local>
2015-12-02 18:59:03 -0500

    added instructions button to main menu

--- FroggerPrototype/src/froggerprot/SplashScreen.java
+++ FroggerPrototype/src/froggerprot/SplashScreen.java
@@ -3,26 +3,26 @@ package froggerprot;
 import javax.swing.JPanel;
 import java.awt.*;
 import javax.swing.*;
-import java.util.*;
-import java.io.File;
 import java.awt.event.*;
-import java.nio.file.Files;
-import java.nio.file.*;
 
 public class SplashScreen extends JPanel implements ActionListener
 {
     final int SCREEN_WIDTH = 800;
     final int SCREEN_HEIGHT = 600;
-    private JButton startButton;
+    private JButton startButton, instructionButton;
     
     public SplashScreen()
     {
         setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         setVisible(true);
         startButton = new JButton("Start Game");
-        startButton.setBounds(0,0,100,25);
+        startButton.setBounds(0,100,100,25);
         startButton.addActionListener(this);
         add(startButton);
+        instructionButton = new JButton("Instructions");
+        instructionButton.setBounds(0,200,100,25);
+        instructionButton.addActionListener(this);
+        add(instructionButton);
     }
     @Override
     public void actionPerformed(ActionEvent ae)
@@ -34,6 +34,14 @@ public class SplashScreen extends JPanel implements ActionListener
             getParent().add(ms);
             setVisible(false);
             disable();
+        }
+        if(o == instructionButton)
+        {
+            JOptionPane.showMessageDialog(this, "Guide the Frog across the road"
+                    + "\nAvoid moving cars"
+                    + "\nIf you get hit you lose a life. You have 3 lives"
+                    + "\nGet across the road and you move on to the next level"
+                    + "\nEach level the speed of the cars increases");
         }
     }
 }
